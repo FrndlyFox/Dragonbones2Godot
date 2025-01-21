61b6cdc44f85f2d51a081d7398b39909f3ea2a2c
F3ynmanium <feynfox@gmail.com>
2025-01-21 18:46:08 +0300

    add simple image import

--- script.gd
+++ script.gd
@@ -20,6 +20,7 @@ func import_pressed() -> void:
 
 
 func process_import(res: Resource) -> void:
+	var res_path = res.resource_path
 	var src = res.data
 	var root = Node2D.new()
 
@@ -40,22 +41,31 @@ func process_import(res: Resource) -> void:
 			bone.position = Vector2(src_bone.transform.x if src_bone.transform.has("x") else 0, src_bone.transform.y if src_bone.transform.has("y") else 0)
 			bone.rotation_degrees = src_bone.transform.skX if src_bone.transform.has("skX") else 0
 			bones_names.append(src_bone.name)
-			bones[src_bone.name] = {}
-			bones[src_bone.name].src = src_bone
-			bones[src_bone.name].bone = bone
+			bones[src_bone.name] = {src = src_bone, bone = bone}
 		
 		print(bones_names)
 		print(bones)
-		bones_names.reverse()
+		# bones_names.reverse()
 		for bname in bones_names:
 			bones[bones[bname].src.parent].bone.add_child(bones[bname].bone)
 		for bname in bones_names:
 			bones[bname].bone.owner = root
 
-
-
-
-
+		var slot_names = {}
+		for slot in src_arm.slot:
+			slot_names[slot.name] = slot.parent
+		for src_slot in src_arm.skin[0].slot:
+			var slot = Sprite2D.new()
+			slot.name = src_slot.name
+			slot.position = Vector2(src_slot.display[0].transform.x if src_slot.display[0].transform.has("x") else 0, 
+															src_slot.display[0].transform.y if src_slot.display[0].transform.has("y") else 0)
+			slot.rotation_degrees = src_slot.display[0].transform.skX if src_slot.display[0].transform.has("skX") else 0
+			bones[slot_names[src_slot.name]].bone.add_child(slot)
+			slot.owner = root
+			
+			# var texture = Texture2D.new()
+			slot.texture = load(res_path.substr(0, res_path.rfind("_"))+"_texture/"+src_slot.display[0].name+".png")
+			# print(res_path.substr(0, res_path.rfind("_"))+"_texture/"+src_slot.name+".png")
 
 
 
