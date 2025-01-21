91f3c5ccc263a4b3b37dabf99b1354e7062559db
F3ynmanium <feynfox@gmail.com>
2025-01-21 05:13:16 +0300

    minimal import, bones w/o any other elements

--- db-import.gd
+++ db-import.gd
@@ -7,11 +7,12 @@ func _enter_tree() -> void:
 	dock = preload("res://addons/db-import/dock.tscn").instantiate()
 	var picker = EditorResourcePicker.new()
 	dock.get_node("VBoxContainer/ModelSelector").add_child(picker)
+	picker.name = "EditorResourcePicker"
 	picker.base_type = "JSON"
 	picker.size_flags_horizontal = 3
 	add_control_to_dock(DOCK_SLOT_RIGHT_BL, dock)
 	preload("./reloader.gd").new()
-	print("db-import initialized")
+	# print("db-import initialized")
 
 func _exit_tree() -> void:
 	remove_control_from_docks(dock)
--- script.gd
+++ script.gd
@@ -1,14 +1,11 @@
 @tool
 extends Control
 
-var model_path
-var src
-# @onready var path_picker = $VBoxContainer/ModelSelector/EditorResoucePicker
+@onready var picker = $VBoxContainer/ModelSelector/EditorResourcePicker
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,3 +14,58 @@ func _process(delta: float) -> void:
 
 func import_pressed() -> void:
 	print("import pressed")
+	if picker.edited_resource:
+		print(type_string(typeof( picker.edited_resource )))
+		process_import(picker.edited_resource)
+
+
+func process_import(res: Resource) -> void:
+	var src = res.data
+	var root = Node2D.new()
+
+	for src_arm in src.armature:
+		var arm = Node2D.new()
+		arm.name = src_arm.name
+		root.add_child(arm)
+		arm.owner = root
+
+		var src_root = src_arm.bone[0].name
+		var bones = {}
+		bones[src_root] = {bone=arm}
+		var bones_names = []
+		for bone_i in range(1, len(src_arm.bone)):
+			var src_bone = src_arm.bone[bone_i]
+			var bone = Node2D.new()
+			bone.name = src_bone.name
+			bone.position = Vector2(src_bone.transform.x if src_bone.transform.has("x") else 0, src_bone.transform.y if src_bone.transform.has("y") else 0)
+			bone.rotation_degrees = src_bone.transform.skX if src_bone.transform.has("skX") else 0
+			bones_names.append(src_bone.name)
+			bones[src_bone.name] = {}
+			bones[src_bone.name].src = src_bone
+			bones[src_bone.name].bone = bone
+		
+		print(bones_names)
+		print(bones)
+		bones_names.reverse()
+		for bname in bones_names:
+			bones[bones[bname].src.parent].bone.add_child(bones[bname].bone)
+		for bname in bones_names:
+			bones[bname].bone.owner = root
+
+
+
+
+
+
+
+
+
+
+
+
+	var scene = PackedScene.new()
+	var result = scene.pack(root)
+	if result == OK:
+		var error = ResourceSaver.save(scene, res.resource_path.get_basename()+".tscn")  # Or "user://..."
+		if error != OK:
+			push_error(error)
