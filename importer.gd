dbf515ae11b205474356a0aee6172f8693c72841
F3ynmanium <feynfox@gmail.com>
2025-02-25 20:15:11 +0300

    add atlas import

--- importer.gd
+++ importer.gd
@@ -4,60 +4,81 @@ extends Control
 # ROADMAP
 # curves
 
-@onready var picker = $VBoxContainer/ModelSelector/EditorResourcePicker
+@onready var modelpicker = %Main/Properties/Properties/Model
+@onready var atlaspicker = %Main/Properties/Properties/Atlas
+
+func _atlas_mode_toggled(toggled_on: bool) -> void:
+	%Main/Properties/Properties/Atlas.visible = toggled_on
+	%Main/Properties/Labels/Atlas.visible = toggled_on
 
 func _import_pressed() -> void:
-	if picker.edited_resource:
-		process_import(picker.edited_resource)
+	if modelpicker.edited_resource:
+		process_import(modelpicker.edited_resource,
+			%Main/Properties/Properties/AtlasMode.button_pressed,
+			atlaspicker.edited_resource)
 
-func process_import(res: Resource) -> void:
-	var res_path = res.resource_path
-	var src = res.data
+func process_import(model_res: Resource, atlas_mode: bool = false, atlas_res: Resource = null) -> void:
+	var model_path = model_res.resource_path
+	var model = model_res.data
+	var atlas
+	var atlas_path
+	if atlas_mode:
+		atlas = atlas_res.data
+		atlas_path = model_path.get_base_dir()+"/"+atlas.imagePath
 
 	# make root
-	var src_arm = src.armature[0]
+	var model_arm = model.armature[0]
 	var arm = Node2D.new()
-	arm.name = src_arm.name
-	var src_root = src_arm.bone[0].name
+	arm.name = model.name
+	var model_root = model_arm.bone[0].name
 
 	# make bones
 	var bones = {}
-	bones[src_root] = {bone=arm}
+	bones[model_root] = {bone=arm}
 	var bones_list = []
-	for bone_i in range(1, len(src_arm.bone)):
-		var src_bone = src_arm.bone[bone_i]
+	for bone_i in range(1, len(model_arm.bone)):
+		var model_bone = model_arm.bone[bone_i]
 		var bone = Node2D.new()
-		bone.name = src_bone.name
-		bone.position = Vector2(src_bone.transform.x if src_bone.transform.has("x") else 0, src_bone.transform.y if src_bone.transform.has("y") else 0)
-		bone.rotation_degrees = src_bone.transform.skX if src_bone.transform.has("skX") else 0
-		bones_list.append(src_bone.name)
-		bones[src_bone.name] = {src = src_bone, bone = bone}
+		bone.name = model_bone.name
+		bone.position = Vector2(model_bone.transform.x if model_bone.transform.has("x") else 0, model_bone.transform.y if model_bone.transform.has("y") else 0)
+		bone.rotation_degrees = model_bone.transform.skX if model_bone.transform.has("skX") else 0
+		bones_list.append(model_bone.name)
+		bones[model_bone.name] = {model = model_bone, bone = bone}
 	
 	# sort bones
 	for bone_name in bones_list:
-		bones[bones[bone_name].src.parent].bone.add_child(bones[bone_name].bone)
+		bones[bones[bone_name].model.parent].bone.add_child(bones[bone_name].bone)
 		bones[bone_name].bone.owner = arm
 	# bones bones bones
 
 	# make slots (images)
+	var atlas_regions = {}
+	if atlas_mode:
+		for slot in atlas.SubTexture:
+			atlas_regions[slot.name] = Rect2(slot.x, slot.y, slot.width, slot.height)
 	var slots = {}
-	for slot_i in range(len(src_arm.slot)):
-		var slot = src_arm.slot[slot_i]
+	for slot_i in range(len(model_arm.slot)):
+		var slot = model_arm.slot[slot_i]
 		slots[slot.name] = {parent = slot.parent, z = slot_i}
-	for src_slot in src_arm.skin[0].slot:
+	for model_slot in model_arm.skin[0].slot:
 		var slot = Sprite2D.new()
-		slot.name = src_slot.name
-		slot.position = Vector2(src_slot.display[0].transform.x if src_slot.display[0].transform.has("x") else 0, 
-														src_slot.display[0].transform.y if src_slot.display[0].transform.has("y") else 0)
-		slot.rotation_degrees = src_slot.display[0].transform.skX if src_slot.display[0].transform.has("skX") else 0
-		bones[slots[src_slot.name].parent].bone.add_child(slot)
+		slot.name = model_slot.name
+		slot.position = Vector2(model_slot.display[0].transform.x if model_slot.display[0].transform.has("x") else 0, 
+														model_slot.display[0].transform.y if model_slot.display[0].transform.has("y") else 0)
+		slot.rotation_degrees = model_slot.display[0].transform.skX if model_slot.display[0].transform.has("skX") else 0
+		bones[slots[model_slot.name].parent].bone.add_child(slot)
 		slot.owner = arm
-		slot.texture = load(res_path.substr(0, res_path.rfind("_"))+"_texture/"+src_slot.display[0].name+".png")
 		slot.z_as_relative = false
-		slot.z_index = slots[src_slot.name].z
+		slot.z_index = slots[model_slot.name].z
+		if atlas_mode:
+			slot.texture = load(atlas_path)
+			slot.region_enabled = true
+			slot.region_rect = atlas_regions[model_slot.display[0].name]
+		else:
+			slot.texture = load(model_path.substr(0, model_path.rfind("_"))+"_texture/"+model_slot.display[0].name+".png")
 
 	# animations
-	var src_anims = src_arm.animation
+	var model_anims = model_arm.animation
 
 	var animplayer = AnimationPlayer.new()
 	var animlib = AnimationLibrary.new()
@@ -67,24 +88,24 @@ func process_import(res: Resource) -> void:
 	arm.add_child(animplayer)
 	animplayer.owner = arm
 
-	for src_anim in src_anims:
-		print(src_anim.name)
+	for model_anim in model_anims:
+		print(model_anim.name)
 		var reset = animlib.get_animation("RESET")
 		var anim = Animation.new()
-		anim.set_length(src_anim.duration / src_arm.frameRate)
-		anim.set_step(1 / src_arm.frameRate)
-		if src_anim.playTimes == 0:
+		anim.set_length(model_anim.duration / model_arm.frameRate)
+		anim.set_step(1 / model_arm.frameRate)
+		if model_anim.playTimes == 0:
 			anim.set_loop_mode(1)
-		for src_bone in src_anim.bone:
-			var path = src_bone.name
-			var bone_parent = src_bone.name
-			while bones[bone_parent].src.parent != src_root:
-				bone_parent = bones[bone_parent].src.parent
+		for model_bone in model_anim.bone:
+			var path = model_bone.name
+			var bone_parent = model_bone.name
+			while bones[bone_parent].model.parent != model_root:
+				bone_parent = bones[bone_parent].model.parent
 				path = bone_parent+"/"+path
-			print("\t" + src_bone.name + ":\t" + path)
+			print("\t" + model_bone.name + ":\t" + path)
 
-			if src_bone.has("translateFrame"):
-				var def_pos = bones[src_bone.name].bone.position
+			if model_bone.has("translateFrame"):
+				var def_pos = bones[model_bone.name].bone.position
 				var track = anim.add_track(0)
 				anim.track_set_path(track, path+":position")
 				if reset.find_track(path+":position", 0) < 0:
@@ -92,17 +113,17 @@ func process_import(res: Resource) -> void:
 					reset.track_set_path(reset_track, path+":position")
 					reset.track_insert_key(reset_track,0,def_pos)
 				var time = 0
-				for frame in src_bone.translateFrame:
+				for frame in model_bone.translateFrame:
 					var new_pos = def_pos
 					if frame.has("x"):
 						new_pos.x += frame.x
 					if frame.has("y"):
 						new_pos.y += frame.y
-					anim.track_insert_key(track,time/src_arm.frameRate,new_pos)
+					anim.track_insert_key(track,time/model_arm.frameRate,new_pos)
 					time += frame.duration if frame.has("duration") else 1
 
-			if src_bone.has("rotateFrame"):
-				var def_rot = bones[src_bone.name].bone.rotation
+			if model_bone.has("rotateFrame"):
+				var def_rot = bones[model_bone.name].bone.rotation
 				var track = anim.add_track(0)
 				anim.track_set_path(track, path+":rotation")
 				if reset.find_track(path+":rotation", 0) < 0:
@@ -110,14 +131,14 @@ func process_import(res: Resource) -> void:
 					reset.track_set_path(reset_track, path+":rotation")
 					reset.track_insert_key(reset_track,0,def_rot)
 				var time = 0
-				for frame in src_bone.rotateFrame:
+				for frame in model_bone.rotateFrame:
 					var new_rot = def_rot
 					if frame.has("rotate"):
 						new_rot += deg_to_rad(frame.rotate)
-					anim.track_insert_key(track,time/src_arm.frameRate,new_rot)
+					anim.track_insert_key(track,time/model_arm.frameRate,new_rot)
 					time += frame.duration if frame.has("duration") else 1
 
-			animlib.add_animation(src_anim.name, anim)
+			animlib.add_animation(model_anim.name, anim)
 
 
 
@@ -127,8 +148,9 @@ func process_import(res: Resource) -> void:
 	var scene = PackedScene.new()
 	var result = scene.pack(arm)
 	if result == OK:
-		var error = ResourceSaver.save(scene, res.resource_path.get_basename()+".tscn")  # Or "user://..."
+		var error = ResourceSaver.save(scene, model_path.get_basename()+".tscn")  # Or "user://..."
 		if error != OK:
 			push_error(error)
 		else:
-			print(res_path + " successfully imported")
+			print(model_path + " successfully imported")
+
--- init.gd
+++ init.gd
@@ -5,11 +5,6 @@ var dock
 
 func _enter_tree() -> void:
 	dock = preload("res://addons/Dragonbones2Godot/dock.tscn").instantiate()
-	var picker = EditorResourcePicker.new()
-	dock.get_node("VBoxContainer/ModelSelector").add_child(picker)
-	picker.name = "EditorResourcePicker"
-	picker.base_type = "JSON"
-	picker.size_flags_horizontal = 3
 	add_control_to_dock(DOCK_SLOT_RIGHT_BL, dock)
 	preload("./reloader.gd").new()
 	# print("db-import initialized")
