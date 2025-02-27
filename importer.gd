962421850a96fd10b54ef4671ad6a843cfe4b54c
F3ynmanium <feynfox@gmail.com>
2025-02-27 20:14:22 +0300

    remaking skins/slots and making displayFrame keyframes support

--- importer.gd
+++ importer.gd
@@ -51,7 +51,7 @@ func process_import(model_res: Resource, atlas_mode: bool = false, atlas_res: Re
 		bones[bone_name].bone.owner = arm
 	# bones bones bones
 
-	# make slots (images)
+	# parse atlas regions, slots and skins
 	var atlas_regions = {}
 	if atlas_mode:
 		for slot in atlas.SubTexture:
@@ -59,23 +59,50 @@ func process_import(model_res: Resource, atlas_mode: bool = false, atlas_res: Re
 	var slots = {}
 	for slot_i in range(len(model_arm.slot)):
 		var slot = model_arm.slot[slot_i]
-		slots[slot.name] = {parent = slot.parent, z = slot_i}
+		slots[slot.name] = {display_index = slot.displayIndex if slot.has("displayIndex") else 0,
+												parent = slot.parent, z = slot_i}
+
+	var skins = []
+	for model_skin in model_arm.skin:
+		var skin = {}
+		skin.slots = {}
+		for model_slot in model_skin.slot:
+			var slot = {}
+			slot.displays = []
+			for model_display in model_slot.display:
+				var display = {pos = Vector2.ZERO, scl = Vector2.ONE, rot = 0}
+				if model_display.has("transform"):
+					display.pos = Vector2(model_display.transform.x if model_display.transform.has("x") else 0,
+																model_display.transform.y if model_display.transform.has("y") else 0)
+					display.scl = Vector2(model_display.transform.scX if model_display.transform.has("scX") else 1,
+																model_display.transform.scY if model_display.transform.has("scY") else 1)
+					display.rot = deg_to_rad(model_display.transform.skX) if model_display.transform.has("skX") else 0
+
+				slot.displays.append(display)
+				skin.slots[model_slot.name] = slot
+		skins.append(skin)
+	Log.prn(skins)
+
+	# make slots
 	for model_slot in model_arm.skin[0].slot:
 		var slot = Sprite2D.new()
 		slot.name = model_slot.name
-		slot.position = Vector2(model_slot.display[0].transform.x if model_slot.display[0].transform.has("x") else 0, 
-														model_slot.display[0].transform.y if model_slot.display[0].transform.has("y") else 0)
-		slot.rotation_degrees = model_slot.display[0].transform.skX if model_slot.display[0].transform.has("skX") else 0
 		bones[slots[model_slot.name].parent].bone.add_child(slot)
 		slot.owner = arm
 		slot.z_as_relative = false
 		slot.z_index = slots[model_slot.name].z
+		if slots[model_slot.name].display_index == -1:
+			continue
+		var display = model_slot.display[slots[model_slot.name].display_index]
+		slot.position = Vector2(display.transform.x if display.transform.has("x") else 0, 
+														display.transform.y if display.transform.has("y") else 0)
+		slot.rotation_degrees = display.transform.skX if display.transform.has("skX") else 0
 		if atlas_mode:
 			slot.texture = load(atlas_path)
 			slot.region_enabled = true
-			slot.region_rect = atlas_regions[model_slot.display[0].name]
+			# slot.region_rect = atlas_regions[display.name]
 		else:
-			slot.texture = load(model_path.substr(0, model_path.rfind("_"))+"_texture/"+model_slot.display[0].name+".png")
+			slot.texture = load(model_path.substr(0, model_path.rfind("_"))+"_texture/"+display.name+".png")
 
 	# animations
 	var model_anims = model_arm.animation
@@ -89,20 +116,22 @@ func process_import(model_res: Resource, atlas_mode: bool = false, atlas_res: Re
 	animplayer.owner = arm
 
 	for model_anim in model_anims:
-		print(model_anim.name)
+		# print(model_anim.name)
 		var reset = animlib.get_animation("RESET")
 		var anim = Animation.new()
 		anim.set_length(model_anim.duration / model_arm.frameRate)
 		anim.set_step(1 / model_arm.frameRate)
 		if model_anim.playTimes == 0:
 			anim.set_loop_mode(1)
-		for model_bone in model_anim.bone:
+
+#-- bones ---------------------------------------------------------------------
+		for model_bone in (model_anim.bone if model_anim.has("bone") else []):
 			var path = model_bone.name
 			var bone_parent = model_bone.name
 			while bones[bone_parent].model.parent != model_root:
 				bone_parent = bones[bone_parent].model.parent
 				path = bone_parent+"/"+path
-			print("\t" + model_bone.name + ":\t" + path)
+			# print("\t" + model_bone.name + ":\t" + path)
 
 			if model_bone.has("translateFrame"):
 				var def_pos = bones[model_bone.name].bone.position
@@ -138,7 +167,23 @@ func process_import(model_res: Resource, atlas_mode: bool = false, atlas_res: Re
 					anim.track_insert_key(track,time/model_arm.frameRate,new_rot)
 					time += frame.duration if frame.has("duration") else 1
 
-			animlib.add_animation(model_anim.name, anim)
+#-- slots ---------------------------------------------------------------------
+		for model_slot in (model_anim.slot if model_anim.has("slot") else []):
+			var path = slots[model_slot.name].parent+"/"+model_slot.name
+			var slot_parent = slots[model_slot.name].parent
+			while bones[slot_parent].model.parent != model_root:
+				slot_parent = bones[slot_parent].model.parent
+				path = slot_parent+"/"+path
+			# print(model_slot.name+": "+path)
+			if model_slot.has("displayFrame"):
+				var def_tex = bones[slots[model_slot.name].parent].bone.find_child(model_slot.name).texture
+				var track = anim.add_track(0)
+				anim.track_set_path(track, path+":texture")
+				if reset.find_track(path+":texture", 0) < 0:
+					var reset_track = reset.add_track(0)
+					reset.track_set_path(reset_track, path+":texture")
+					reset.track_insert_key(reset_track,0,def_tex)
+		animlib.add_animation(model_anim.name, anim)
 
 
 
